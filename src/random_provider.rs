456e0720d4afcbe045d85c7837e8695de093a406
Dustin Toff <me@dustintoff.com>
2025-03-20 19:43:51 -0400

    Upgrade rust and deps
    Summary:
    
    Test Plan:

--- src/random_provider.rs
+++ src/random_provider.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-use rand::prelude::*;
+use rand;
 
 pub trait RandomProvider {
     /// Returns a random value between lower and upper bound, inclusive.
@@ -90,15 +90,15 @@ pub struct DefaultRandomProvider {}
 
 impl RandomProvider for DefaultRandomProvider {
     fn pick_linear_u64(&self, lower_bound: u64, upper_bound: u64) -> u64 {
-        thread_rng().gen_range(lower_bound..=upper_bound)
+        rand::random_range(lower_bound..=upper_bound)
     }
 
     fn pick_linear_i64(&self, lower_bound: i64, upper_bound: i64) -> i64 {
-        thread_rng().gen_range(lower_bound..=upper_bound)
+        rand::random_range(lower_bound..=upper_bound)
     }
 
     fn pick_linear_usize(&self, lower_bound: usize, upper_bound: usize) -> usize {
-        thread_rng().gen_range(lower_bound..=upper_bound)
+        rand::random_range(lower_bound..=upper_bound)
     }
 }
 
